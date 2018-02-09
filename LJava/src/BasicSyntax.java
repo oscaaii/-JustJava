0a91984c69f937df9dff98aa136f0ec10ebfade5
oscaaii <thebiggestfoolever123@gmail.com>
2018-02-09 17:18:50 +0100

    Add files via upload

---
+++ LJava/src/BasicSyntax.java
@@ -0,0 +1,18 @@
+
+public class BasicSyntax {
+	public static void main(String[] args) {
+		
+		//Variable Type name ( = value); 
+		
+		int youreNigga = 42;
+		
+		System.out.println( youreNigga);
+		
+		
+		youreNigga = 929;
+		System.out.println( youreNigga);
+ 
+		
+	}
+
+}
---
+++ LJava/src/Operations.java
@@ -0,0 +1,52 @@
+
+public class Operations {
+	
+	public static void main(String [] args) {
+		
+		
+		/* Add +
+		 * Subtract -
+		 * Multiply *
+		 * Divide / 
+		 */
+		int a, b, c, d;
+		
+		a = 43;
+		b = 42; 
+	
+		
+		c = (a + b) / 10; 
+		
+		d = 2 * c; 
+		
+		System.out.println(c + d);
+		
+		// 8bit number - byte
+		byte verySmallNumber;
+		
+		// 16bit number - short
+	short smallNumber;
+	
+	// 32bit will be fine for most calculations 
+	int averageNumber;
+	
+	// 64bit - very large number, largest primitive number in normal java otherwise there's some custom classes in java.
+	long largeNumber;
+	
+	//decimalnumber
+	
+	// 32bit put a f on the end to tell the compiler that it should be a float type decimal number
+	float Averagedecimal = 1.02329667f;
+	
+	// large - long version of the float - 64bit - really precise numbers
+	double largeDecimal = 112223.222223467;
+	
+	// true or false value
+	boolean trueorfalse = true;
+	
+	// character valuetype - can be anything on the keyboard - when declaring a char use single qoute marks
+	char character = 'd'; 
+	}
+
+	
+}
